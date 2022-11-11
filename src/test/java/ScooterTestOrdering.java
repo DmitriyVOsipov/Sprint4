10899d862645f0aae5cf859fe0df703dcc08af32
DmitriyVOsipov <osipovdmitriy@yandex.ru>
2022-11-11 18:01:11 +0300

    Проект переделал в компактный вид на 1 тест

--- src/test/java/ScooterTestOrdering.java
+++ src/test/java/ScooterTestOrdering.java
@@ -1,121 +1,69 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import pageobjects.ScooterHomePage;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.junit.Assert.assertTrue;
+import static org.openqa.selenium.By.cssSelector;
 import pageobjects.ScooterOrderPage;
 
+import java.util.concurrent.TimeUnit;
+
+@RunWith(Parameterized.class)
 public class ScooterTestOrdering {
-    @Before
-    public void startUp() {
-        WebDriverManager.chromedriver().setup();
+
+    WebDriver driver = new ChromeDriver();
+    //WebDriver driver = new FirefoxDriver();
+
+    final By buttonAddress;
+    final String name;
+    final String family;
+    final String address;
+    final String numberPhone;
+    final String comment;
+
+    public ScooterTestOrdering(By buttonAddress, String name, String family, String address, String numberPhone, String comment) {
+        this.buttonAddress = buttonAddress;
+        this.name = name;
+        this.family = family;
+        this.address = address;
+        this.numberPhone = numberPhone;
+        this.comment = comment;
     }
-    private WebDriver driver;
-
-    // Данные  Заказа
-    private final String name = "Дмитрий";
-    private final String surname = "Осипов";
-    private final String address = "Щёлково, Пролетарский проспект дом 3, квартира 159";
-    private final String phone = "89250914255";
-    private final String metroStations = "Черкизовская";
-    private final String whenDate = "11.11.2022";
-    private final String commentCourier = "Позвонить за час до приезда";
-    private final String textExpected = "Заказ оформлен";
-
-    @Test
-    // Путь. Верхняя кнопка "Заказать" Google Chrome
-    public void pressTopButtonOrdering() {
-        // драйвер Google Chrome
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments();
-        driver = new ChromeDriver(options);
-
-        ScooterHomePage objHomePage = new ScooterHomePage(driver);
-        objHomePage.openGetHomePage();
-        objHomePage.clickTopButtonOrder();
-
-        ScooterOrderPage objOrderPage = new ScooterOrderPage(driver);
-        objOrderPage.fillingFieldsFirstPage(name, surname, address, phone, metroStations);
-        objOrderPage.clickButtonFarther();
-        objOrderPage.fillingFieldsSecondPage(whenDate, commentCourier);
-        objOrderPage.clickButtonOrder();
-        objOrderPage.clickButtonYes();
-        objOrderPage.waitTextAppear(textExpected);
+    @Parameterized.Parameters
+    public static Object[][] Parameters() {
+        return new Object[][] {
+                {cssSelector("div.Header_Nav__AGCXC > button.Button_Button__ra12g"), "Осипов", "Дмитрий", "Щелково Пролетарский проспект дом 3 квартира 159", "89250914255", "Позвонить за час до доставки"},
+                {cssSelector("div.Home_RoadMap__2tal_ > div.Home_FinishButton__1_cWm > button"), "Осипов", "Дмитрий", "Щелково Пролетарский проспект дом 3 квартира 159", "89250914255", "Позвонить за час до доставки"}
+        };
     }
-
     @Test
-    // Путь. Нижняя кнопка "Заказать" Google Chrome
-    public void pressBottomButtonOrdering() {
-        // драйвер Google Chrome
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments();
-        driver = new ChromeDriver(options);
-
-        ScooterHomePage objHomePage = new ScooterHomePage(driver);
-        objHomePage.openGetHomePage();
-        objHomePage.scrollBottomButtonOrder();
-        objHomePage.clickBottomButtonOrder();
-
-        ScooterOrderPage objOrderPage = new ScooterOrderPage(driver);
-        objOrderPage.fillingFieldsFirstPage(name, surname, address, phone, metroStations);
-        objOrderPage.clickButtonFarther();
-        objOrderPage.fillingFieldsSecondPage(whenDate, commentCourier);
-        objOrderPage.clickButtonOrder();
-        objOrderPage.clickButtonYes();
-        objOrderPage.waitTextAppear(textExpected);
-    }
-
-    @Test
-    // Путь Верхняя кнопка "Заказать" FireFox
-    public void pressTopButtonOrderingFireFox() {
-        // драйвер Firefox
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments();
-        driver = new FirefoxDriver(options);
-
-        ScooterHomePage objHomePage = new ScooterHomePage(driver);
-        objHomePage.openGetHomePage();
-        objHomePage.clickTopButtonOrder();
-
-        ScooterOrderPage objOrderPage = new ScooterOrderPage(driver);
-        objOrderPage.fillingFieldsFirstPage(name, surname, address, phone, metroStations);
-        objOrderPage.clickButtonFarther();
-        objOrderPage.fillingFieldsSecondPage(whenDate, commentCourier);
-        objOrderPage.clickButtonOrder();
-        objOrderPage.clickButtonYes();
-        objOrderPage.waitTextAppear(textExpected);
-    }
-
-    @Test
-    // Путь. Нижняя кнопка "Заказать" FireFox
-    public void pressBottomButtonOrderingFireFox() {
-        // драйвер Firefox
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments();
-        driver = new FirefoxDriver(options);
-
-        ScooterHomePage objHomePage = new ScooterHomePage(driver);
-        objHomePage.openGetHomePage();
-        objHomePage.scrollBottomButtonOrder();
-        objHomePage.clickBottomButtonOrder();
-
-        ScooterOrderPage objOrderPage = new ScooterOrderPage(driver);
-        objOrderPage.fillingFieldsFirstPage(name, surname, address, phone, metroStations);
-        objOrderPage.clickButtonFarther();
-        objOrderPage.fillingFieldsSecondPage(whenDate, commentCourier);
-        objOrderPage.clickButtonOrder();
-        objOrderPage.clickButtonYes();
-        objOrderPage.waitTextAppear(textExpected);
+    public void checkOrderFromHeader_success() {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        ScooterOrderPage scooterOrderPagePage = new ScooterOrderPage(driver);
+        scooterOrderPagePage.startPage();
+        scooterOrderPagePage.findCheckAndClickOrderButton(buttonAddress);
+        scooterOrderPagePage.userName(name);
+        scooterOrderPagePage.userSurname(family);
+        scooterOrderPagePage.userAddress(address);
+        scooterOrderPagePage.userPhone(numberPhone);
+        scooterOrderPagePage.metroStation();
+        scooterOrderPagePage.metroStationChoice();
+        scooterOrderPagePage.nextPageButton();
+        scooterOrderPagePage.calendarDate();
+        scooterOrderPagePage.rentalDuration();
+        scooterOrderPagePage.scooterColour();
+        scooterOrderPagePage.userComment(comment);
+        scooterOrderPagePage.orderButton();
+        scooterOrderPagePage.confirmButton();
+        assertTrue("ERROR", scooterOrderPagePage.CreatedOrder());
     }
 
     @After
-    public void teardown() {
-        // Закрой браузер
+    public void closedBrowser() {
         driver.quit();
     }
 }
